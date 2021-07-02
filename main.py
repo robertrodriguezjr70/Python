7e0562f39603101779c96399d8a11330e60a16be
Robert Rodriguez <robert.rodriguez.jr.70@gmail.com>
2021-07-02 14:48:57 -0700

    Python Skeleton file

---
+++ main.py
@@ -0,0 +1,20 @@
+
+#imports here
+import utility
+from shopping.scart import shopping_cart,shopping_cart2
+
+
+#test for main
+if __name__ == "__main__":
+    print('please print this')
+    print(__name__)
+
+while True:
+    try:
+        pass
+    except:
+        pass
+    else:
+        pass
+    finally:
+        pass
